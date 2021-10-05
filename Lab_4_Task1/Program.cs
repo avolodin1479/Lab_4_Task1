fbad8ab335c3426781103f0258e2bfcd595ad8e4
User <User@USER-PC>
2021-10-05 22:11:29 +0600

    Добавьте файлы проекта.

---
+++ Lab_4_Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое положительное число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int b=0
+
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine("Квадрат {0} равен {1}", i, i+=1 + (i*2-1));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
